8e3ec4ccd9181add4ae4e535e47534e17c34df63
bmuraya <babumuraya@gmail.com>
2023-09-08 22:03:55 +0300

    console

--- file.js
+++ file.js
@@ -1,3 +1,14 @@
 // log a statment using console.log()
 
 console.log('Hello from Script')
+
+// Variables 
+let age = 25
+// document.write(age);
+console.log(age)
+
+const salary = 8000
+// document.write(salary);
+console.log(salary)
+
+let s
