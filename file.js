d2270ec9cd818091fc93165259ec300733b0ded9
bmuraya <babumuraya@gmail.com>
2023-09-10 11:26:11 +0300

    JavaScript Data Types

--- file.js
+++ file.js
@@ -3,12 +3,137 @@
 console.log('Hello from Script')
 
 // Variables 
-let age = 25
-// document.write(age);
-console.log(age)
+// Variables are Containers for Storing Data
+// JavaScript Variables can be declared in 4 ways:
 
-const salary = 8000
-// document.write(salary);
-console.log(salary)
+// Automatically
+// Using var
+// Using let
+// Using const
 
-let sum 
+
+// // Automatically
+// x = 52;
+// y = 1;
+// z = x + y;
+// console.log(z)
+
+
+// // Using var
+// var j = 5;
+// var k = 6;
+// var u = j + k;
+// console.log(u)
+
+
+// var x = "John Doe";
+
+// var x = 0;
+// console.log(x)
+
+// let  x = 10;
+// // Here x is 10
+
+// {  
+// let x = 2;
+// // Here x is 2
+// }
+
+// console.log(x)
+
+
+// // You can create a const object:
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// // You can change a property:
+// car.color = "red";
+
+// // You can add a property:
+// car.owner = "Johnson";
+
+// console.log( car.owner, car.model, car.color, car.type)
+
+// let age = 25
+// // document.write(age);
+// console.log(age)
+
+// const salary = 8000 
+// // salary = 80000
+// // document.write(salary);
+// console.log(salary)
+
+// let sum = 0
+// sum = 5
+// console.log(sum)
+
+
+// JavaScript Data Types
+
+// primitive type    
+//     String
+//     Number
+//     Boolean
+//     undefined
+//     null
+//     biglnt
+//     Symbol
+
+// Non primitive type 
+//    Ojbects
+
+
+// strings
+
+// string the following  goutes
+
+const name = "muraya"
+const language= 'javaScript'
+const chanel =  `murayach`
+
+
+// Number 
+
+const total = 0
+const PI =3.14
+
+// Boolean
+
+// use to run code for true or fasle statment 
+
+const isPrimaryNumber =true
+const IsNewUser = false
+
+
+// undefined 
+//  value that is not assisgned 
+
+let result
+console.log(result)
+
+const res =null //explicitly 
+
+// null
+// special value represent unknown value or empty value in JS
+
+const data=null
+
+
+// const object store value 
+const person =
+{
+    firstName: 'Muraya',
+    lastName: 'babu',
+    age: 30
+}
+
+console.log(person.firstName, person.lastName, person.age)
+
+const oodNumbers = [1, 3, 5, 7, 9]
+console.log(oodNumbers[4])
+
+
+let a=10
+a= 'babu'
+a=true
+
+console.log(a)
